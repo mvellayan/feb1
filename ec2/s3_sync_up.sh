5481672ec3c7ebf3e947eb990709e71b05b13817
Muthu Vellayan <muthu.vellayan@gmail.com>
2026-04-20 17:39:09 -0400

    Refactoring. and removing tmp and examples

--- ec2/s3_sync_up.sh
+++ ec2/s3_sync_up.sh
@@ -1,4 +1,7 @@
 aws s3 sync ~/Development/feb1 s3://arbo1/feb1/ \
   --exclude "data/options/*" \
   --exclude ".git/*" \
+  --exclude "paper1/*" \
+  --exclude "*/reports/*" \
+  --exclude "example_references/*" \
   --exclude ".venv/*"
