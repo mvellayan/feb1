a4c4e860a4b5afb3c427f701fc696b20f46c32dd
Muthu Vellayan <muthu.vellayan@gmail.com>
2026-05-10 19:20:50 -0400

    commiting sync up/down scripts

--- ec2/s3_sync_down.sh
+++ ec2/s3_sync_down.sh
@@ -1,1 +1,2 @@
+echo "`date`: syncing down from s3"
 aws s3 sync s3://arbo1/feb1/ ~/Development/feb1 
--- ec2/s3_sync_up.sh
+++ ec2/s3_sync_up.sh
@@ -1,3 +1,4 @@
+echo "`date`: syncing up to s3..."
 aws s3 sync ~/Development/feb1 s3://arbo1/feb1/ \
   --exclude "data/options/*" \
   --exclude ".git/*" \
